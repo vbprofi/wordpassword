05036a1ad9dc37fc2faf3073d8edbeea8519af7e
vbprofi <vb-profi@mail.ru>
2017-06-10 11:43:24 +0200

    alle project files

---
+++ wordpasswort/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace wordpasswort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                        Object oMissing = System.Reflection.Missing.Value;
+
+            bool Found = false;
+
+            PasswordGenerator Generator = new PasswordGenerator(true, true, true, 1, 4);
+            Thread.Sleep(1000);
+            string pw = Generator.Dequeue();
+
+            Word.Application WordApp = new Word.Application();
+            Word.Document ExistingDocument;
+
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+            String Dateiname = file.FileName;
+
+            while (pw != "" && !Found)
+            {
+                try
+                {
+                    ExistingDocument = WordApp.Documents.Open(Dateiname, oMissing, false, oMissing, pw);
+                    Found = true;
+                    WordApp.Quit();
+                    MessageBox.Show("Das Passwort lautet: " + pw);
+                }
+                catch (System.Runtime.InteropServices.COMException ex)
+                {
+                }
+                pw = Generator.Dequeue();
+            }
+
+            if (!Found)
+                MessageBox.Show("Kein passendes Passwort gefunden.");
+        
+ 
+        }
+
+    }
+}
---
+++ wordpasswort/PasswordGenerator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading;                
+
+public class PasswordGenerator
+{
+    Queue<string> Results; // Warteschlange mit erzeugten Passwörtern
+
+    bool UpperAZ; // true wenn Großbuchstaben benutzt werden sollen
+    bool LowerAZ; // true wenn Kleinbuchstaben benutzt werden sollen
+    bool Numbers; // true wenn Zahlen benutzt werden sollen
+
+    int Min; // minimale Länge des Passworts
+    int Max; // maximale Länge des Passworts
+
+    const int MaxQueueSize = 30000000; // maximale Zahl von Passwörtern, die gleichzeitig in der Queue gespeichert werden dürfen (Schutz vor Speicherüberlauf)
+
+    public PasswordGenerator(bool upperAZ, bool lowerAZ, bool numbers, int min, int max)
+    {
+        UpperAZ = upperAZ;
+        LowerAZ = lowerAZ;
+        Numbers = numbers;
+
+        Min = min;
+        Max = max;
+        Results = new Queue<string>();
+
+        // neuen Thread zur Passwortgenerierung erstellen und starten
+        Thread Creator = new Thread(new ThreadStart(FillQueue));
+        Creator.Start();
+    }
+
+    private void FillQueue()
+    {
+        // Warteschlange füllen
+        for (int i = Min; i <= Max; i++)
+        {
+            SetLetter(i, 0, "");
+        }
+    }
+
+    private void SetLetter(int length, int pos, string temp)
+    {
+        // aktuelle Position mit allen möglichen Zeichen füllen
+        if (UpperAZ)
+        {
+            for (int i = 65; i <= 90; i++)
+            {
+                NextStep(length, pos, temp + (char)i);
+            }
+        }
+        if (LowerAZ)
+        {
+            for (int i = 97; i <= 122; i++)
+            {
+                NextStep(length, pos, temp + (char)i);
+            }
+        }
+        if (Numbers)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                NextStep(length, pos, temp + i.ToString());
+            }
+        }
+    }
+
+    private void NextStep(int length, int pos, string temp)
+    {
+        // Funktion zum Abschließen eines Schrittes
+
+        // ist die Warteschlange "voll", wird der Thread pausiert
+        while (Results.Count > MaxQueueSize)
+            Thread.Sleep(500);
+
+        // ist noch nicht die Endposition im Passwort erreicht, wird SetLetters() mit der nächsten Position aufgerufen
+        if (pos < length - 1)
+            SetLetter(length, pos + 1, temp);
+        else
+            // ansonsten wird das aktuelle Passwort der Warteschlange hinzugefügt
+            Results.Enqueue(temp);
+    }
+
+    public string Dequeue()
+    {
+        // liefert das unterste Element der Warteschlange
+        if (Results.Count > 0)
+            return Results.Dequeue();
+        else
+            return "";
+    }
+}
+
